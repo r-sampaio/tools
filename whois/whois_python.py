ef082129b618ad6c675c010accdb4450b04328bc
r-sampaio <16281210+r-sampaio@users.noreply.github.com>
2021-02-06 00:40:44 -0300

    v2

--- whois/whois_python.py
+++ whois/whois_python.py
@@ -3,9 +3,28 @@
 
 import socket
 import sys
+import os
+
+def limpa():
+    """ Limpa a tela do terminal """
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+limpa()
 
 openSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 openSocket.connect(("whois.iana.org",43))
 openSocket.sendall((f"{sys.argv[1]}\r\n").encode())
 resposta = openSocket.recv(1024)
-print((resposta.decode()).split())
+resp = ((resposta.decode()).split())[19]
+openSocket.close()
+
+openSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+openSocket.connect((resp,43))
+openSocket.sendall((f"{sys.argv[1]}\r\n").encode())
+resposta = openSocket.recv(1024)
+print(resposta.decode(encoding='utf-8', errors='ignore'))
+openSocket.close()
