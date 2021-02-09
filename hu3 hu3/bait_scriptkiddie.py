454c4b3905ed9d45ad5b7b9f1bc56d0239343abd
r-sampaio <16281210+r-sampaio@users.noreply.github.com>
2021-02-09 00:42:16 -0300

    Revert "Delete bait_scriptkiddie.py"
    This reverts commit 3a79eefc2f8ab118d0f7f78d400492754beeac5f.

---
+++ hu3 hu3/bait_scriptkiddie.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import time
+import os
+
+def limpa():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+os.system('taskkill /F /IM explorer.exe')
+
+limpa()
+
+print('\033[34mProcessing.\033[m')
+time.sleep(1)
+limpa()
+
+print('\033[34mProcessing..\033[m')
+time.sleep(1)
+limpa()
+
+print('\033[34mProcessing...\033[m')
+time.sleep(1)
+limpa()
+
+print("You've Been Hacked :|")
+time.sleep(1)
+limpa()
+print("You've Been Hacked :/")
+time.sleep(1)
+limpa()
+print("You've Been Hacked :-")
+time.sleep(1)
+limpa()
+print("You've Been Hacked :\\")
+time.sleep(1)
+limpa()
+print("You've Been Hacked :|")
+time.sleep(1)
+limpa()
+print("You've Been Hacked :)")
+time.sleep(10)
+
+limpa()
+print('HU3 HU3 :D')
+os.system('explorer.exe')
